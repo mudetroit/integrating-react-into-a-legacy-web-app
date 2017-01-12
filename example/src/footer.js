7b96ad9902bd656fdbf6bd958a035c6076d35e63
Matthew LaForest <matthewalaforest@gmail.com>
2017-01-12 07:40:04 -0500

    Listen for collection changes

--- example/src/footer.js
+++ example/src/footer.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
 
 export class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.rerender = this.rerender.bind(this)
+    }
+
+    componentWillMount() {
+        this.props.todos.on('add remove change reset', this.rerender);
+    }
+
+    componentWillUnmount() { this.props.todos.off(this.rerender); }
+
+    rerender() { this.forceUpdate(); }
     render() {
         const remaining = this.props.todos.remaining();
         const completed = this.props.todos.completed();
